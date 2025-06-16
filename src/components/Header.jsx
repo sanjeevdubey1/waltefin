7c1e0f98eec5bb3333cd0d2685a5f58a71041224
sanjeevdubey1 <93029995+sanjeevdubey1@users.noreply.github.com>
2025-06-16 23:46:09 +0530

    added the updated aboutus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,8 +53,8 @@ export const Header = () => {
         <Link to="/" className="block text-pink-100 py-2 hover:text-yellow-500 font-bold">Home</Link>
         <Link to="/service" className="block text-pink-100 py-2 hover:text-yellow-500 font-bold">Services</Link>
         <Link to="/about" className="block text-pink-100 py-2 hover:text-yellow-500 font-bold">About Us</Link>
+        <Link className="block text-pink-100 py-2 hover:text-yellow-500 font-bold" to="/contactus">Contact Us</Link>
         <Link to="/terms" className="block text-pink-100 py-2 hover:text-yellow-500 font-bold">T&amp;C</Link>
-        <Link className="block text-pink-100 py-2 hover:text-yellow-500 font-bold" to="#contactus">Contact Us</Link>
       </div>
     </header>
   );
